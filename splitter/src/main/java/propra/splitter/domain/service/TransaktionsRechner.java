1867f59b6beb5ba8a60ad2b6e77c2a56228e687e
Taha El Amine Kassabi <taha.kassabi@hhu.de>
2023-03-10 13:49:00 +0100

    Add MoneyWrapper & Liability Calculation

--- splitter/src/main/java/propra/splitter/domain/service/TransaktionsRechner.java
+++ splitter/src/main/java/propra/splitter/domain/service/TransaktionsRechner.java
@@ -1,10 +1,23 @@
 package propra.splitter.domain.service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import propra.splitter.domain.model.Ausgabe;
-import propra.splitter.domain.model.Transaktion;
 
-public interface TransaktionsRechner {
+public class TransaktionsRechner {
 
-  Set<Transaktion> rechne(Set<Ausgabe> expenses);
+  public static Set<Liability> liabilities(Set<Ausgabe> expenses) {
+    Map<String, MoneyWrapper> liabilities = new HashMap<>();
+    for (Ausgabe ausgabe : expenses) {
+      MoneyWrapper share = MoneyWrapper.ofCents(ausgabe.cents()).divide(ausgabe.anzahlTeilnehmer());
+      for (String participant : ausgabe.schuldner()) {
+        liabilities.compute(participant, (p, m) -> m == null ? share.negate() : m.subtract(share));
+      }
+      liabilities.compute(ausgabe.glauebiger(),
+          (p, m) -> m == null ? MoneyWrapper.ofCents(ausgabe.cents())
+              : m.addCents(ausgabe.cents()));
+    }
+    return Liability.mapToSet(liabilities);
+  }
 }
